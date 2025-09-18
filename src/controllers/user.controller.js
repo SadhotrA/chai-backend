3f2020fc574a70508453843e49030472a1d64094
SadhotrA <vishalsadhotra00@gmail.com>
2025-09-18 23:42:05 +0530

    bb

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,12 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req, res)=>{
-  
+    //get user details from front end 
+    //validation -not empty
+    //check if user already exists:check username | email
+    //check for image and avatar
+    //upload them to cloudinary
+    //
   }
 )
 
